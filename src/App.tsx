580e4506ff858f28e8544d1a6174bb6039f7b44e
Kundan Kumar <39196236+Nukealbert@users.noreply.github.com>
2023-05-25 16:49:45 +0530

    This is a task app based on react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import InputField from './components/InputField';
+
+
+
+const App:React.FC =()=> {
+  return (
+   <div className='App'>
+    <span className="heading">Tasker</span>
+    <InputField />
+   </div>
+  );
+}
+
+export default App;
